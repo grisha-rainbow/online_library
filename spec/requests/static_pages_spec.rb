fc984b6537c5d95907f801e7e10691c7ee6685ee
rainbow <grisharozhnov@gmail.com>
2014-04-22 08:52:19 +0300

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "Online library" }
+
   describe "Home page" do
     it "should have the content 'Online library'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -10,7 +13,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Online library | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -23,7 +26,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Online library | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -36,7 +39,20 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Online library | About")
+      expect(page).to have_title("#{base_title} | About")
     end
+  end
+
+  describe "Contact Us" do
+
+      it "Should have the content 'Contact Us'" do
+        visit '/static_pages/contact'
+        expect(page).to have_content("Contact Us")
+      end
+
+      it "should have the right title" do
+        visit '/static_pages/contact'
+        expect(page).to have_title("Online library | Contact Us")
+      end
   end
 end
