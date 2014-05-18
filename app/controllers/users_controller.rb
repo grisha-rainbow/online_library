01e585d5ecbcfd7bd73e633e5ee0949f1ab85a98
rainbow <grisharozhnov@gmail.com>
2014-05-18 21:06:14 +0300

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params) 
   	if @user.save
+      sign_in @user 
   		flash[:success] = "Вітаємо на сторінці Онлайн бібліотеки!"
       redirect_to @user
   	else
